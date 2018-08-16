5ec0daff1cda1af76d16a1dfc9a7667178a42265
孙文祺 <arthursun2005@gmail.com>
2018-08-16 17:49:35 +1000

    Initial Commit

---
+++ Foggy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Foggy
+//
+//  Created by Arthur Sun on 8/16/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
